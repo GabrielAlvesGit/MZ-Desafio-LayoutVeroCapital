60a092ad02d18804cedc89bf8044df625c7c7495
GabrielAlvesGit <gabrielalves.-100@hotmail.com>
2025-06-16 01:34:47 -0300

    pre slick - config slick + banner

--- js/slick.js
+++ js/slick.js
@@ -1,1 +1,12 @@
-$('.banners').slick();
+$(document).ready(function(){
+  $('.js-slicks').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    // autoplay: true,
+    // autoplaySpeed: 2000,
+  });
+});
+		
