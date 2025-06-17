369399f65d13f9ee10d1543938d7184831d161e2
GabrielAlvesGit <gabrielalves.-100@hotmail.com>
2025-06-17 01:53:51 -0300

    section nossas soluções slick pre

--- js/slick.js
+++ js/slick.js
@@ -8,5 +8,34 @@ $(document).ready(function(){
     // autoplay: true,
     // autoplaySpeed: 2000,
   });
+
+$('.js-slicks-ourSolutions').slick({
+  centerMode: true,
+  slidesToShow: 3,
+  arrows: true,
+  dots: false,
+  infinite: true,
+  speed: 500,
+  variableWidth: true,
+  adaptiveHeight: true,
+  prevArrow: '<button class="slick-prev">❮</button>',
+  nextArrow: '<button class="slick-next">❯</button>',
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        centerMode: true,
+        slidesToShow: 1,
+        variableWidth: false
+      }
+    }
+  ]
 });
+
+
+
+});
+
+
+
 		
