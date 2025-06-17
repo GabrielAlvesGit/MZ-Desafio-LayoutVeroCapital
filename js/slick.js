d0b13c93afe8f221fb767518adab5774befe9775
GabrielAlvesGit <gabrielalves.-100@hotmail.com>
2025-06-17 19:32:21 -0300

    section Nossas Solucoes Slick End

--- js/slick.js
+++ js/slick.js
@@ -9,31 +9,16 @@ $(document).ready(function(){
     // autoplaySpeed: 2000,
   });
 
-$('.js-slicks-ourSolutions').slick({
-  centerMode: true,
-  slidesToShow: 3,
-  arrows: true,
-  dots: false,
-  infinite: true,
-  speed: 500,
-  variableWidth: true,
-  adaptiveHeight: true,
-  prevArrow: '<button class="slick-prev">❮</button>',
-  nextArrow: '<button class="slick-next">❯</button>',
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        centerMode: true,
-        slidesToShow: 1,
-        variableWidth: false
-      }
-    }
-  ]
-});
-
-
-
+  $('.js-slicks-ourSolutions').slick({
+    centerMode: true,
+    slidesToShow: 3,
+    arrows: true,
+    dots: false,
+    infinite: true,
+    speed: 500,
+    variableWidth: true,
+    adaptiveHeight: true,
+  });
 });
 
 
