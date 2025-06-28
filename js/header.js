5959a6108837a79076fbfefd1b7dd3aae173b08d
GabrielAlvesGit <gabrielalves.-100@hotmail.com>
2025-06-28 01:18:38 -0300

    create menu hamburg + ajustes na section Nossas Soluções no mobile

---
+++ js/header.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+
+  $('.js-menu').click(function () {
+    $('.js-menu-open').toggleClass('active');
+    $('.js-menu-close').toggleClass('active');
+    $('.js-navigation').toggleClass('active');
+  });
+
+});
